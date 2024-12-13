47c9d3c85d049627353162bae3a8b17113fb690c
Alexander Ponimaskin <ponimaskin@gmail.com>
2024-12-13 07:07:46 +0100

    12.2

--- 12.py
+++ 12.py
@@ -13,13 +13,47 @@ visited = [[False for _ in l] for l in m]
 
 p = (0, 0)
 
-deltas = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+deltas = [(-1, 0, 'U'), (1, 0, 'D'), (0, -1, 'L'), (0, 1, 'R')]
 
 positions = deque((y, x) for y in range(len(m)) for x in range(len(m[0])))
 connected = []
 
 
+def count_wallls(d, pp):
+    c = 0
+    while len(pp):
+        p = pp.pop()
+        c += 1
+        while True:
+            if d in {'U', 'D'}:
+                l = (p[0], p[1] - 1)
+                r = (p[0], p[1] + 1)
+                if l in pp:
+                    p = l
+                    pp.remove(l)
+                    continue
+                if r in pp:
+                    p = r
+                    pp.remove(r)
+                    continue
+                break
+            else:
+                u = (p[0] - 1, p[1])
+                d = (p[0] + 1, p[1])
+                if u in pp:
+                    p = u
+                    pp.remove(u)
+                    continue
+                if d in pp:
+                    p = d
+                    pp.remove(d)
+                    continue
+                break
+    return c
+
+
 total = 0
+total2 = 0
 for p in positions:
     if visited[p[0]][p[1]]:
         continue
@@ -30,16 +64,15 @@ for p in positions:
     component = set([p])
     perimeter = 0
 
+    walls = {'U': [], 'D': [], 'L': [], 'R': []}
+
     while len(q):
-        # print("---", v, component, perimeter)
         (y, x) = q.popleft()
         if visited[y][x]:
             continue
         visited[y][x] = True
-        # if v == 'B':
-        #     print('aaaaa')
 
-        for dy, dx in deltas:
+        for dy, dx, d in deltas:
             ny = y + dy
             nx = x + dx
 
@@ -48,15 +81,13 @@ for p in positions:
                     if not visited[ny][nx]:
                         component.add((ny, nx))
                         q.append((ny, nx))
-                else:
-                    perimeter += 1
-            else:
-                perimeter += 1
-            # if v == 'B':
-            #     print((x, y), (nx, ny), m[ny][nx], perimeter)
+                    continue
 
-        # print((x, y), perimeter)
-    print('---', v, len(component), perimeter)
+            perimeter += 1
+            walls[d].append((y, x))
+
+    total2 += len(component) * sum(count_wallls(d, walls[d]) for d in 'UDRL')
     total += len(component) * perimeter
 
 print(total)
+print(total2)
