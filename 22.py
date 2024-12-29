7c53dead7b269fd7112d9217f4c9a58af87826a4
Alexander Ponimaskin <ponimaskin@gmail.com>
2024-12-29 09:11:34 -0500

    profiling + cache 22

--- 22.py
+++ 22.py
@@ -2,7 +2,9 @@
 from math import floor
 from itertools import pairwise, islice
 from collections import deque
+from functools import cache
 from heapq import heappush, nlargest
+from line_profiler import profile
 
 f = "test.22.txt"
 f = "22.input.txt"
@@ -19,6 +21,7 @@ def sliding_window(iterable, n):
         yield tuple(window)
 
 
+@cache
 def transform(x):
     x ^= 64 * x
     x %= 16777216
@@ -29,44 +32,48 @@ def transform(x):
     return x
 
 
-ll = open(f).read().strip().split("\n")
+@profile
+def main():
+    ll = open(f).read().strip().split("\n")
+    s = 0
+    prices = []
 
-s = 0
-prices = []
+    # part 1
+    for l in ll:
+        n = int(l)
+        p = [n % 10]
+        for _ in range(2000):
+            n = transform(n)
+            p.append(n % 10)
+        s += n
+        prices.append(p)
+    print(s)
 
-# part 1
-for l in ll:
-    n = int(l)
-    p = [n % 10]
-    sl = []
-    for _ in range(2000):
-        n = transform(n)
-        p.append(n % 10)
-    s += n
-    prices.append(p)
-print(s)
+    slopes = ([b - a for a, b in pairwise(p)] for p in prices)
+    windows = []
+    keys = set()
 
-slopes = ([b - a for a, b in pairwise(p)] for p in prices)
-windows = []
-keys = set()
+    for i, s in enumerate(slopes):
+        ww = sliding_window(s, 4)
+        window = {}
 
-for i, s in enumerate(slopes):
-    ww = sliding_window(s, 4)
-    window = {}
+        for j, w in enumerate(ww):
+            if w in window:
+                continue
+            keys.add(w)
+            window[w] = prices[i][j + 4]
+        windows.append(window)
 
-    for j, w in enumerate(ww):
-        if w in window:
-            continue
-        keys.add(w)
-        window[w] = prices[i][j + 4]
-    windows.append(window)
+    maximums = []
 
-maximums = []
+    for k in keys:
+        w = [w.get(k, 0) for w in windows]
+        s = sum(w)
+        heappush(maximums, s)
 
-for k in keys:
-    w = [w.get(k, 0) for w in windows]
-    s = sum(w)
-    heappush(maximums, s)
+    # 2174 is too low
+    print(nlargest(1, maximums)[0])
 
-# 2174 is too low
-print(nlargest(1, maximums)[0])
+
+if __name__ == "__main__":
+    main()
