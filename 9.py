b71771794d1e4bddf772c88f4196c878748faa20
Alexander Ponimaskin <ponimaskin@gmail.com>
2024-12-29 09:12:00 -0500

    9.2

--- 9.py
+++ 9.py
@@ -1,50 +1,98 @@
 #!/usr/bin/env python3
-from copy import deepcopy
-
 f = "test.9.txt"
-# f = "9.txt"
+f = "9.txt"
 
 m = []
+last_block = 0
 for i, x in enumerate(open(f).readline().strip()):
     sz = int(x)
     m += ["." if i % 2 else i // 2] * sz
+    if i % 2 == 0:
+        last_block = i // 2
 
-mm = deepcopy(m)
 
+def checksum(mm):
+    s = 0
+    for i, x in enumerate(mm):
+        if x != ".":
+            s += i * int(x)
+            # print(i, int(x), i * int(x), s)
+    print(s)
+
+
+mm = m[:]
 l = 0
-r = len(m) - 1
+
+r = len(mm) - 1
 
 while True:
     if l == r:
         break
-    if m[r] == ".":
+    if mm[r] == ".":
         r -= 1
         continue
-    if m[l] == ".":
-        m[l], m[r] = m[r], m[l]
+    if mm[l] == ".":
+        mm[l], mm[r] = mm[r], mm[l]
     l += 1
 
-s = 0
-for i, x in enumerate(m):
-    if x != ".":
-        s += i * int(x)
-        # print(i, int(x), i * int(x), s)
-print(s)
+checksum(mm)
 
-# m = []
-# for k, g in groupby(mm):
-#     g = list(g)
-#     m.append(len(g), k)
+mm = m[:]
 
-# l = 0
-# r = len(m) - 1
-# while True:
-#     if l == r:
-#         break
-#     if m[r][1] == '.':
-#         r -= 1
-#         continue
-#     if m[l][1] == '.':
-#         if
-#         m[l], m[r] = m[r], m[l]
-#     l += 1
+
+def find_space(start_pos=0):
+    while True:
+        if mm[start_pos] != ".":
+            start_pos += 1
+        else:
+            break
+    end_pos = start_pos
+    while end_pos < len(mm) - 1:
+        if mm[end_pos + 1] == ".":
+            end_pos += 1
+        else:
+            break
+    return start_pos, end_pos
+
+
+def find_block(block_id, end_pos):
+    while True:
+        if mm[end_pos] != block_id:
+            end_pos -= 1
+        else:
+            break
+    start_pos = end_pos
+    while True:
+        if mm[start_pos - 1] == block_id:
+            start_pos -= 1
+        else:
+            break
+    return start_pos, end_pos
+
+
+b = len(mm) - 1
+
+# suboptial solution, but it works
+for block_id in range(last_block, 0, -1):
+    ba, bb = find_block(block_id, b)
+    skip = False
+    free_start = 0
+    while True:
+        aa, ab = find_space(free_start)
+        if ab >= ba:
+            skip = True
+            break
+        if ab - aa < bb - ba:
+            free_start = ab + 1
+            continue
+        break
+    if skip:
+        b = aa
+        continue
+
+    mm[aa : aa + (bb - ba) + 1] = mm[ba : bb + 1]
+    mm[ba : bb + 1] = ["."] * (bb - ba + 1)
+    # print("".join(map(str, mm)))
+    b = ba - 1
+
+checksum(mm)
